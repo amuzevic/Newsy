40d101df7567930f5c7c75267d5a85d18f1ce08f
anmu271434 <extern.anton.muzevic@elektrobit.com>
2019-11-19 14:13:37 +0100

    tiny refactor

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
 require("express-async-errors");
-require("./mongoose");
+require("./startup/mongoose");
 require("./startup/routes")(app);
-
 
 
 process.on("uncaughtException", (ex) => {
@@ -15,8 +14,6 @@ process.on("unhandledRejection", (ex) => {
   console.log(ex);
   process.exit(1);
 })
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
